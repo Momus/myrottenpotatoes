be59f63439e5e4859fad688caec74b730caca792
Dmitri <dmitri@momus.net>
2018-09-16 09:24:06 -0600

    Movin' right along

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,30 @@
-#The movies controller.
+# The movies controller.
 class MoviesController < ApplicationController
   def index
     @movies = Movie.all
   end
+
+  def create
+    params.require(:movie).permit(:title,
+                                  :rating,
+                                  :release_date)
+    @movie = Movie.create!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully created."
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def show
+    @movie = Movie.find params[:id]
+  end
+
+  def update;  end
+
+  def destroy; end
 end
