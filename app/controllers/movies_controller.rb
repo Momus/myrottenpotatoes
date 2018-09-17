22bc70613f4dd4b396d5292358ed4653c8a6d764
Dmitri <dmitri@momus.net>
2018-09-17 12:35:39 -0600

    The basic app (as described in the book) is finished.

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -4,13 +4,17 @@ class MoviesController < ApplicationController
     @movies = Movie.all
   end
 
+  def show
+    @movie = Movie.find params[:id]
+  end
+
   def create
     permitted = params.require(:movie).permit(:movie,
                                               :title,
                                               :rating,
                                               :release_date)
     @movie = Movie.create!(permitted)
-    flash[:notice] = "#{@movie.title} was successfully created."
+    flash[:notice] = "<em>#{@movie.title}</em> was successfully created."
     redirect_to movies_path
   end
 
@@ -22,11 +26,17 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
   end
 
-  def show
+  def update
     @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie].permit!)
+    flash[:notice] = "<em>#{@movie.title}</em> was successfully updated."
+    redirect_to movie_path(@movie)
   end
 
-  def update;  end
-
-  def destroy; end
+  def destroy
+    @movie = Movie.find params[:id]
+    @movie.destroy
+    flash[:warning] = "<em>#{@movie.title}</em> was successfully deleted."
+    redirect_to movies_path
+  end
 end
